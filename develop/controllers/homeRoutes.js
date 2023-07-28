eab97c3741e40d6d5172362da4f7be5254e681da
kitkatyourmom <sarahlridings@gmail.com>
2023-07-27 19:02:00 -0600

    chaning router in homeRoutes to access information from user

--- develop/controllers/homeRoutes.js
+++ develop/controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
 router.get('/', async (req, res) => {
   try {
@@ -34,7 +35,21 @@ router.get('/aboutyou', async (req, res) => {
 
 router.get('/main-news', async (req, res) => {
   try {
-    res.render('mainNews');
+    // in {} have the info to include / exclude information
+    const userData = await User.findByPk(req.session.id, {
+      attributes: { exclude: ['password','email'] },
+    });
+    if(!userData) {
+        res.status(404).json({message: 'No user with this id!'});
+        return;
+    }
+      const user = userData.get({ plain: true });
+      res.render('user', user);
+    // serialize data
+
+    // combine then serialize or combine after?
+
+    res.render('mainNews', {}); // {pass in everything from database}
   } catch (err) {
     res.status(500).json(err);
   }
--- develop/public/js/mainpage.js
+++ develop/public/js/mainpage.js
@@ -1,24 +1,25 @@
-const SourceArticles = require("../../models/SourceArticles");
+// const SourceArticles = require("../../models/SourceArticles");
 
 // tabs
 const tabs = document.querySelectorAll('.tablinks');
-const tabContentBoxes = document.querySelectorAll('.tabcontent')
+const tabContentBoxes = document.querySelectorAll('.tabcontent');
 
 tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(item => item.classList.remove('is-active'))
-        tab.classList.add('is-active');
+  tab.addEventListener('click', () => {
+    tabs.forEach((item) => item.classList.remove('is-active'));
+    console.log('test click');
+    tab.classList.add('is-active');
 
-        const target = tab.dataset.target;
-        tabContentBoxes.forEach(box => {
-            if (box.getAttribute('id') === target) {
-                box.classList.remove('is-hidden')
-            } else {
-                box.classList.add('is-hidden')
-            }
-        })
-    })
-})
+    const { target } = tab.dataset;
+    tabContentBoxes.forEach((box) => {
+      if (box.getAttribute('id') === target) {
+        box.classList.remove('is-hidden');
+      } else {
+        box.classList.add('is-hidden');
+      }
+    });
+  });
+});
 
 // pagination
 
@@ -32,26 +33,26 @@ tabs.forEach((tab) => {
 //     return (req, res, next) => {
 //         const page = parseInt(req.query.page)
 //         const limit = parseInt(req.query.limit)
-    
+
 //         const startIndex = (page - 1) * limit
 //         const endIndex = page * limit
-    
+
 //         const results = {}
-    
+
 //         if (endIndex < model.length) {
 //             results.next = {
 //                 page: page + 1,
 //                 limit: limit
 //             }
 //         }
-    
+
 //         if (startIndex > 0) {
 //             results.previous = {
 //                 page: page - 1,
 //                 limit: limit
 //             }
 //         }
-    
+
 //         // this will be different as well - need to research it
 //         results.results = model.slice(startIndex, endIndex)
 
