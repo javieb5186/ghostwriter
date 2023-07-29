55185dafdafbf2f9e17b7ab1b5b1e6a513910f0d
kitkatyourmom <sarahlridings@gmail.com>
2023-07-29 17:37:29 -0600

    require handlebars in handlebarHelpers

--- develop/controllers/helpers/handlebarHelpers.js
+++ develop/controllers/helpers/handlebarHelpers.js
@@ -1,5 +1,7 @@
 // http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates/
 // handlebars if is equal to
+const Handlebars = require('express-handlebars');
+
 Handlebars.registerHelper('equal', function (lvalue, rvalue, options) {
   if (arguments.length < 3) { throw new Error('Handlebars Helper equal needs 2 parameters'); }
   if (lvalue !== rvalue) {
