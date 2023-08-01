9a4f8180b8c385b2e447c47fcebb15e1ed6eed77
Danny Ford <dannyford11@me.com>
2023-08-01 13:10:48 -0500

    added more regex and exception handling to the article generator script

--- develop/controllers/helpers/generateArticle.js
+++ develop/controllers/helpers/generateArticle.js
@@ -8,36 +8,30 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function runCompletion(title, description, author) {
-  const response = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `Can you rewrite this title <${title}> and this description <${description}> and make up a blog post in the style of ${author}? Can you return it as a json with these fields; title, description, author, blogPost? Just the json response nothing else. Please make all special characters json compliant`,
-    max_tokens: 2000,
-  });
-
-  const post = response.data.choices[0].text;
-  console.log(post);
-
   try {
-    // Extract the relevant information from the post using regular expressions
-    const titleMatch = post.match(/"title":\s*"(.*?)"/);
-    const descriptionMatch = post.match(/"description":\s*"(.*?)"/);
-    const authorMatch = post.match(/"author":\s*"(.*?)"/);
-    const blogPostMatch = post.match(/"blogPost":\s*"(.*?)"/);
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Can you rewrite this title <${title}> and this description <${description}> and make up a blog post in the style of ${author}? Can you return it as a json with these fields; title, description, author, blogPost? Just the json response nothing else. Please make all special characters json compliant`,
+      max_tokens: 2000,
+    });
 
-    // Construct the JSON object
-    const jsonObject = {
-      title: titleMatch ? titleMatch[1] : '',
-      description: descriptionMatch ? descriptionMatch[1] : '',
-      author: authorMatch ? authorMatch[1] : '',
-      blogPost: blogPostMatch ? blogPostMatch[1] : '',
-    };
+    const post = response.data.choices[0].text;
+    console.log(post);
 
+    // Remove non-printable/control characters
+    const cleanText = post.replace(/[\x00-\x1F\x7F-\x9F]/g, '');
+    
+    // Extract the JSON object from the `cleanText` using regular expressions
+    const jsonObjectString = cleanText.match(/{[^]*}/s)[0];
+    const jsonObject = JSON.parse(jsonObjectString);
     console.log(jsonObject);
 
     return jsonObject;
   } catch (error) {
     console.error('Error parsing JSON:', error);
-    return null;
+
+    // Retry the function call
+    return runCompletion(title, description, author);
   }
 }
 
